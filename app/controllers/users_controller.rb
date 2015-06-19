8be0e78c720f892382a4f5a83d259b5675797052
miyaryj <miya.ryj@gmail.com>
2015-06-19 10:10:28 +0900

    Map users entries to calendar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 require 'instagram'
 
+include EntriesHelper
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
     if params[:start_date].present?
-      @date_of_month = Date.parse(params[:start_date])
+      date_of_month = Date.parse(params[:start_date])
     else
-      @date_of_month = Date.today
+      date_of_month = Date.today
     end
+
+    @entries = entries_of_month(@user, date_of_month)
   end
 
   private
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -11,4 +11,14 @@ module EntriesHelper
   def action_to_continue(default: {controller: :entries, action: :new})
     session[:return_to_after_instagram] || default
   end
+
+  def entries_of_month(user, date_of_month)
+    month = date_of_month.to_s.rpartition('-')[0]
+    p month
+    user.entries.where("date like '%#{month}%'")
+  end
+
+  def entries_calendar(entries)
+    Hash[entries.map {|entry| [entry.date, entry]}]
+  end
 end

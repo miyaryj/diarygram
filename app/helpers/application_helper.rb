8f690a8ee44d64fbe6aa7782dab5e72bbbc1e8af
miyaryj <miya.ryj@gmail.com>
2015-06-25 19:25:06 +0900

    Use paper-header-panel for header and tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,15 @@ module ApplicationHelper
     @tab = tab
   end
 
+  def current_tab_index
+    case @tab
+    when :timeline
+      "0"
+    when :calendar
+      "1"
+    end
+  end
+
   def current_tab?(tab)
     @tab == tab
   end

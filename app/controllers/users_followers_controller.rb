88e3b6b230314dffdde904e7df5420538f1a96b0
miyaryj <miya.ryj@gmail.com>
2015-08-18 17:23:34 +0900

    Implement follow button using Ajax

---
+++ app/controllers/users_followers_controller.rb
@@ -0,0 +1,21 @@
+class UsersFollowersController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @user = User.find(params[:users_follower][:followed_id])
+    current_user.follow!(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    @user = UsersFollower.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+end
